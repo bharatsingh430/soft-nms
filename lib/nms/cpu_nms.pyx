bb6913d5a01ff39d31208eb8831ce81259152734
Bharat Singh <bharatsingh430@gmail.com>
2017-04-18 19:50:14 -0400

    fixed import error

--- lib/nms/cpu_nms.pyx
+++ lib/nms/cpu_nms.pyx
@@ -5,6 +5,7 @@
 # Written by Navaneeth Bodla and Bharat Singh
 # ----------------------------------------------------------
 
+import numpy as np
 cimport numpy as np
 
 cdef inline np.float32_t max(np.float32_t a, np.float32_t b):
